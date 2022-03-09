387867bd6f9f7c1f6a74dfeb4bfc747853ebd3cd
Chen Ning <chenningg97@gmail.com>
2022-03-09 16:57:54 +0100

    Upgrade to lwjgl3 for desktop

--- tnig/desktop/src/com/tnig/game/desktop/DesktopLauncher.java
+++ tnig/desktop/src/com/tnig/game/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package com.tnig.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.tnig.game.Tnig;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Tnig(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new Tnig(), config);
 	}
 }
