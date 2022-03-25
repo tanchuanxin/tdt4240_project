ec47d22b627f6f046dfacdf7d37dd3fb5e18f2e0
Chen Ning <chenningg97@gmail.com>
2022-03-25 23:53:11 +0100

    Added Skin and fonts for AssetManager

--- core/src/com/tnig/game/view/screens/LoadingScreen.java
+++ core/src/com/tnig/game/view/screens/LoadingScreen.java
@@ -1,15 +1,21 @@
 package com.tnig.game.view.screens;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.tnig.game.utillities.AssetLoader;
 
 
 public class LoadingScreen extends AbstractScreen {
+    private Stage stage;
+
     public LoadingScreen(OrthographicCamera camera, AssetLoader assetLoader) {
         super(camera, assetLoader);
+        stage = new Stage(new ScreenViewport());
 
         // Load loading screen assets synchronously
-        assetLoader.getManager().load(assetLoader.BAD_LOGIC_SPLASH);
+        assetLoader.getManager().load(assetLoader.FONT_SOURCE_SANS_PRO_REGULAR);
+        assetLoader.getManager().load(assetLoader.IMG_SPLASH_SCREEN_BG);
         assetLoader.getManager().finishLoading();
 
         // Load all assets asynchronously
@@ -25,11 +31,11 @@ public class LoadingScreen extends AbstractScreen {
     public void render(float delta) {
         // If we are done loading, go to main menu screen.
         if(assetLoader.getManager().update()) {
-            // Unload splash screen assets as we won't use them again
-            assetLoader.getManager().unload(assetLoader.BAD_LOGIC_SPLASH.fileName);
-
             // Go to main menu screen
             ScreenManager.getInstance().setMainMenuScreen();
+
+            // Unload splash screen assets as we won't use them again
+            assetLoader.getManager().unload(assetLoader.IMG_SPLASH_SCREEN_BG.fileName);
         }
 
         // Display loading information
