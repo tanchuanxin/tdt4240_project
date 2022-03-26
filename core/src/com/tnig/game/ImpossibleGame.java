823068f375cd278a38492ab2d4e0397a811146f2
Chen Ning <chenningg97@gmail.com>
2022-03-26 10:23:19 +0100

    Refactored ScreenManager

--- core/src/com/tnig/game/ImpossibleGame.java
+++ core/src/com/tnig/game/ImpossibleGame.java
@@ -2,6 +2,7 @@ package com.tnig.game;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.tnig.game.view.screens.Screen;
 import com.tnig.game.view.screens.ScreenManager;
 
 public class ImpossibleGame extends Game {
@@ -12,7 +13,7 @@ public class ImpossibleGame extends Game {
 	public void create () {
 		// Initialize screen manager and set it to loading screen while we load assets
 		ScreenManager.getInstance().initialize(this);
-		ScreenManager.getInstance().setLoadingScreen();
+		ScreenManager.getInstance().setScreen(Screen.LOADING);
 	}
 
 	@Override
---
+++ core/src/com/tnig/game/view/screens/Screen.java
@@ -0,0 +1,7 @@
+package com.tnig.game.view.screens;
+
+public enum Screen {
+    LOADING,
+    MAIN_MENU,
+    GAME
+}
