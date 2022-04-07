76252268dda5af08cf576200483773fd46f07f0d
Ruben Kroken Rokkones <rubenkro@stud.ntnu.no>
2022-04-07 10:49:45 +0200

    Leaderboards working. Ther is bug in loop at Eventmanager l:62. Getting ConcurrentModificationExc without able to find the trigger.

--- core/src/com/tnig/game/view/guis/LeaderboardsScreenGUI.java
+++ core/src/com/tnig/game/view/guis/LeaderboardsScreenGUI.java
@@ -12,42 +12,34 @@ import com.tnig.game.utilities.events.Event;
 import com.tnig.game.utilities.events.EventListener;
 import com.tnig.game.utilities.events.EventManager;
 import com.tnig.game.utilities.events.EventName;
-import com.tnig.game.utilities.events.LeaderBoardSelectedEvent;
-import com.tnig.game.utilities.events.MapSelectedEvent;
 import com.tnig.game.utilities.events.ViewLeaderboardsEvent;
-import com.tnig.game.utilities.events.ViewMainMenuEvent;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class LeaderboardsScreenGUI extends AbstractGUI implements EventListener {
-    private final EventManager eventManager;
-    private Network network;
-    private final List<Button> mapBtnList = new ArrayList<>();
+
     private int mapNum = 1;
 
     public LeaderboardsScreenGUI(OrthographicCamera camera, AssetLoader assetLoader, final EventManager eventManager, Network network) {
         super(camera, assetLoader);
-        this.eventManager = eventManager;
-        this.network = network;
         eventManager.subscribe(EventName.VIEW_LEADERBOARDS, this);
         Table table = new Table();
 
-        Label selectMap = new Label("Leaderboard" + mapNum, assetLoader.get(assetLoader.SKIN_PIXTHULHU_UI));
+        Label selectMap = new Label("Leaderboard" + mapNum, assetLoader.get(AssetLoader.SKIN_PIXTHULHU_UI));
 
-        Label backBtnLabel = new Label("Back", assetLoader.get(assetLoader.SKIN_PIXTHULHU_UI));
-        final Button backBtn = new Button(backBtnLabel, assetLoader.get(assetLoader.SKIN_PIXTHULHU_UI));
+        Label backBtnLabel = new Label("Back", assetLoader.get(AssetLoader.SKIN_PIXTHULHU_UI));
+        final Button backBtn = new Button(backBtnLabel, assetLoader.get(AssetLoader.SKIN_PIXTHULHU_UI));
         backBtn.addListener(new ClickListener() {
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 return true;
-            };
+            }
 
             @Override
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
                 // Change screen to map select screen
                 eventManager.pushEvent(new ViewLeaderboardsEvent());
-            };
+            }
         });
 
         // Add actors to table layout
@@ -61,7 +53,7 @@ public class LeaderboardsScreenGUI extends AbstractGUI implements EventListener 
             counter++;
             table.row().spaceBottom(20f);
             for (String user : internalList) {
-                table.add(new Label(user, assetLoader.get(assetLoader.SKIN_PIXTHULHU_UI)));
+                table.add(new Label(user, assetLoader.get(AssetLoader.SKIN_PIXTHULHU_UI)));
             }
             if (counter >= 9) break;
         }
@@ -78,5 +70,6 @@ public class LeaderboardsScreenGUI extends AbstractGUI implements EventListener 
     @Override
     public void receiveEvent(Event event) {
         mapNum = (int) event.data.get("mapNum");
+        System.out.println("Receiving event ---------------------------------");
     }
 }
--- core/src/com/tnig/game/view/guis/SelectLeaderBoardScreenGUI.java
+++ core/src/com/tnig/game/view/guis/SelectLeaderBoardScreenGUI.java
@@ -17,55 +17,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SelectLeaderBoardScreenGUI extends AbstractGUI{
-    private final EventManager eventManager;
-    private final List<Button> mapBtnList = new ArrayList<>();
-    private Network network;
-    private ArrayList<Integer> levelNums;
 
     public SelectLeaderBoardScreenGUI(OrthographicCamera camera, AssetLoader assetLoader, final EventManager eventManager, Network network) {
         super(camera, assetLoader);
-        this.eventManager = eventManager;
-        this.network = network;
 
-        levelNums = new ArrayList<>(network.getLevels());
+        ArrayList<Integer> levelNum = new ArrayList<>(network.getLevels());
         Table table = new Table();
 
         // Create actors
-        Label titleLabel = new Label("Select map leaderboard", assetLoader.get(assetLoader.SKIN_PIXTHULHU_UI));
+        Label titleLabel = new Label("Select map leaderboard", assetLoader.get(AssetLoader.SKIN_PIXTHULHU_UI));
         titleLabel.setAlignment(Align.center);
 
-        for (Integer map = 1; map <= levelNums.size(); map++) {
-            Button mapBtn = new Button(new Label(map.toString(), assetLoader.get(assetLoader.SKIN_PIXTHULHU_UI)), assetLoader.get(assetLoader.SKIN_PIXTHULHU_UI));
+        List<Button> mapBtnList = new ArrayList<>();
+        for (int map = 1; map <= levelNum.size(); map++) {
+            Button mapBtn = new Button(new Label(Integer.toString(map), assetLoader.get(AssetLoader.SKIN_PIXTHULHU_UI)), assetLoader.get(AssetLoader.SKIN_PIXTHULHU_UI));
             final Integer mapNum = map;
             mapBtn.addListener(new ClickListener() {
                 @Override
                 public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                     return true;
-                };
+                }
 
                 @Override
                 public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
                     // Change screen to game screen
-                    System.out.println("Map selected: " + mapNum.toString());
+                    System.out.println("Map selected: " + mapNum);
                     eventManager.pushEvent(new LeaderBoardSelectedEvent(mapNum));
-                };
+                }
             });
             mapBtnList.add(mapBtn);
         }
 
-        Label backBtnLabel = new Label("Back", assetLoader.get(assetLoader.SKIN_PIXTHULHU_UI));
-        final Button backBtn = new Button(backBtnLabel, assetLoader.get(assetLoader.SKIN_PIXTHULHU_UI));
+        Label backBtnLabel = new Label("Back", assetLoader.get(AssetLoader.SKIN_PIXTHULHU_UI));
+        final Button backBtn = new Button(backBtnLabel, assetLoader.get(AssetLoader.SKIN_PIXTHULHU_UI));
         backBtn.addListener(new ClickListener() {
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 return true;
-            };
+            }
 
             @Override
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
                 // Change screen to map select screen
                 eventManager.pushEvent(new ViewMainMenuEvent());
-            };
+            }
         });
 
         // Add actors to table layout
