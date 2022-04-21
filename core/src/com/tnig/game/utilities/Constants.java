91e0d2124103e56ff9c814104bf0cb0dd4cc107d
Chen Ning <chenningg97@gmail.com>
2022-04-21 11:08:23 +0200

    Removed GameRenderer

--- core/src/com/tnig/game/utilities/Constants.java
+++ core/src/com/tnig/game/utilities/Constants.java
@@ -1,10 +1,10 @@
 package com.tnig.game.utilities;
 
 public class Constants {
-    public static final float PPM = 32f;
+    public static final float PPM = 16f;
     public static final int FPS = 60;
-    public static final float VIEWPORT_WIDTH = 1000f;
-    public static final float VIEWPORT_HEIGHT = 580f;
+    public static final float VIEWPORT_WIDTH = 400f;
+    public static final float VIEWPORT_HEIGHT = 200f;
 
     public static final float WORLD_WIDTH = 320f;
     public static final float WORLD_HEIGHT = 180f;
