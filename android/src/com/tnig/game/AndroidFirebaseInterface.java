e8f8f1a2298df469449475d2ba62b26224d8d7ce
Chen Ning <chenningg97@gmail.com>
2022-04-24 17:19:01 +0200

    Fix bug of leaderboards level being incorrect

--- android/src/com/tnig/game/AndroidFirebaseInterface.java
+++ android/src/com/tnig/game/AndroidFirebaseInterface.java
@@ -74,11 +74,8 @@ public class AndroidFirebaseInterface implements NetworkService {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 if (dataSnapshot.exists()) {
-                    //Iterate over the levels.
-                    int mapNum = 0;
-
                     for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                        mapNum++;
+                        int mapNum = Integer.parseInt(String.valueOf(ds.getKey().charAt(ds.getKey().length() - 1)));
                         players = new ArrayList<>();
 
                         // Iterate over the player records in the level.
